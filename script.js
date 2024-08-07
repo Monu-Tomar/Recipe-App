c15b6553f6b627e536e7c3a3c3d4bcc1abe2d812
Monu-Tomar <monusinghtomar107@gmail.com>
2024-08-07 16:12:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const popupBtn = document.getElementById('popup-btn');
+const popup = document.getElementById('popup');
+const closeBtn = document.getElementById('close-btn');
+const overlay = document.getElementById('overlay');
+
+
+popupBtn.addEventListener('click', () => {
+    popup.style.display = 'block';
+    overlay.style.display = 'block';
+
+});
+closeBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+    overlay.style.display = 'block';
+});
+
+
+overlay.addEventListener('click', () => {
+    popup.style.display = 'none';
+    overlay.style.display = 'none';
+});
+
+const popupMenu = document.getElementById('popup-menu');
+popupMenu.addEventListener('click', () => {
+    popup.style.display = 'block';
+    overlay.style.display = 'block';
+});
+
+var disableBodyScroll = function(){
+    window.body_scroll_pos = $(window).scrollTop(); 
+    $('body').css('overflow-y','hidden');
+}
+
+var enableBodyScroll = function(){
+    $('body').css('overflow-y','scroll');
+    $(window).scrollTop(window.body_scroll_pos); 
+}
